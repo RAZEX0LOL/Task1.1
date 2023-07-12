1ad70233e9dbbcfa761f1864d217482f5206f268
Rasul Khattaev <99183060+RAZEX0LOL@users.noreply.github.com>
2023-07-12 21:59:17 +0400

    Second commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ class App extends React.Component {
         }
         this.inputClick = this.inputClick.bind(this)
     }
+    componentDidUpdate(prevProp) {
+        if(this.state.helpText!=="help"){
+            console.log("Some")
+        }
+    }
 
     render() {
         return (
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,6 +11,7 @@ class Header extends React.Component {
             </header>
         )
     }
+
 }
 
 export default Header
