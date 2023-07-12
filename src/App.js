270d7f6180e406e5820ce38f17df0796aafc968b
Rasul Khattaev <99183060+RAZEX0LOL@users.noreply.github.com>
2023-07-12 21:54:21 +0400

    Second commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import logo from './img/logo.png';
 class App extends React.Component {
     constructor(props) {
         super(props)
-        this.state={
-            helpText:"Help Text",
+        this.state = {
+            helpText: "Help Text",
             userData: ""
 
         }
-        this.inputClick=this.inputClick.bind(this)
+        this.inputClick = this.inputClick.bind(this)
     }
 
     render() {
@@ -21,16 +21,16 @@ class App extends React.Component {
                 <h1>{this.state.helpText}</h1>
                 <h2>{this.state.userData}</h2>
                 <input placeholder={this.state.helpText}
-                       onChange={event => this.setState({userData:event.target.value})}
+                       onChange={event => this.setState({userData: event.target.value})}
                        onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
                 <p>{this.state.helpText === "Help Text" ? "Yes" : "No"}</p>
-                <Image image={logo} />
+                <Image image={logo}/>
             </div>
         )
     }
 
     inputClick() {
-        this.setState({helpText:"Changed"})
+        this.setState({helpText: "Changed"})
         console.log("Clicked")
     }
 
