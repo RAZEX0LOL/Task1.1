8e2f3f46bb6f00097b2d44324343c7bf1b227340
Rasul Khattaev <99183060+RAZEX0LOL@users.noreply.github.com>
2023-07-12 22:54:51 +0400

    commit 5

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import {IoCloseCircleSharp} from "react-icons/io5";
 
 class User extends React.Component {
     user=this.props.user
     render() {
         return (
             <div className="user" >
+                <IoCloseCircleSharp className={"delete-icon"}/>
                 <h3>{this.user.firstname} {this.user.lastname}</h3>
                 <p>{this.user.bio}</p>
                 <b>{this.user.isHappy ? 'Счастлив': 'Он черт, какой счастлив'}</b>
